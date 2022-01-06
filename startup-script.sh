9ff50ee55a7911af6527a5f4c005f4bc4fdb18c8
Roman Vavilov <monter1990@gmail.com>
2022-01-06 12:34:05 +0000

    Add startup-script.sh & remove app.yaml

---
+++ startup-script.sh
@@ -0,0 +1,53 @@
+set -v
+
+
+# Talk to the metadata server to get the project id
+PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
+REPOSITORY="wiki_api"
+
+# Install logging monitor. The monitor will automatically pick up logs sent to
+# syslog.
+curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
+service google-fluentd restart &
+
+# Install dependencies from apt
+apt-get update
+apt-get install -yq ca-certificates git build-essential supervisor ripgrep
+
+# Install nodejs
+mkdir /opt/nodejs
+curl https://nodejs.org/dist/v16.13.1/node-v16.13.1-linux-x64.tar.xz | tar xvzf - -C /opt/nodejs --strip-components=1
+ln -s /opt/nodejs/bin/node /usr/bin/node
+ln -s /opt/nodejs/bin/npm /usr/bin/npm
+
+# Get the application source code from the Google Cloud Repository.
+# git requires $HOME and it's not set during the startup script.
+export HOME=/root
+git config --global credential.helper gcloud.sh
+git clone https://source.cloud.google.com/sixth-storm-337007/github_vavilov2212_wiki_api /opt/app/wiki_api
+
+# Install app dependencies
+cd /opt/app/wiki_api
+npm install
+
+# Create a nodeapp user. The application will run as this user.
+useradd -m -d /home/nodeapp nodeapp
+chown -R nodeapp:nodeapp /opt/app
+
+# Configure supervisor to run the node app.
+cat >/etc/supervisor/conf.d/wiki_api.conf << EOF
+[program:wiki_api]
+directory=/opt/app/wiki_api
+command=npm start
+autostart=true
+autorestart=true
+user=nodeapp
+environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production"
+stdout_logfile=syslog
+stderr_logfile=syslog
+EOF
+
+supervisorctl reread
+supervisorctl update
+
+# Application should now be running under supervisor
